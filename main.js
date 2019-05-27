373cf7f244b1c97675691523cfe97ad207cfcb9c
Szymon Warszawa <szymonwarszawa@gmail.com>
2019-05-27 21:57:17 +0100

    ading elements to an array (for of loop and forEach())

---
+++ main.js
@@ -0,0 +1,32 @@
+const names = [];
+const div = document.querySelector('div');
+
+const addName = (e) => {
+    e.preventDefault();
+    const input = document.querySelector('input');
+    const newName = input.value;
+    if (input.value) {
+        //for of loop
+        for (name of names) {
+            if (name === newName) {
+                alert('this name has been added already!');
+                return
+            }
+        }
+        // forEach method
+        // names.forEach((name) => {
+        //     if (name === newName) {
+        //         alert('no double names allowed!');
+        //         return
+        //     }
+        // })
+
+        names.push(newName);
+        div.textContent += `${newName}, `;
+        input.value = ''
+    }
+    console.log('dalej leci')
+}
+
+
+document.querySelector('button').addEventListener('click', addName)
